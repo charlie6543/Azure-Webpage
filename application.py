aa34a8a74741c430ed430773ce827ab79839723a
charlie6543 <114709092+charlie6543@users.noreply.github.com>
2024-03-05 00:11:23 -0800

    Update application.py

--- application.py
+++ application.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def init_interface():
     return '''<h1>this is a website<h1>
-    <button type="button">Load Data</button>
-    <button type="button">Clear Data</button>
+    <button type="button">Load Data</button><br>
+    <button type="button">Clear Data</button><br>
     <label for="fName">First Name</label>
     <input type="text" id'="fName" name="fName"/>
     <label for="lName">Last Name</label>
