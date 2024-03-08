28d0d51ec165875a06baadf22a0eb59760f11082
charlie6543 <114709092+charlie6543@users.noreply.github.com>
2024-03-08 15:09:46 -0800

    Adding functionality

--- application.py
+++ application.py
@@ -6,83 +6,43 @@ import uuid
 import http.client
 
 from azure.core.exceptions import AzureError
-from azure.cosmos import CosmosClient, PartitionKey
+#from azure.cosmos import CosmosClient, PartitionKey
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
-
-DB_NAME = "li-c-436p4-server"
-COLLECTION_NAME = "Collection1"
+from pymongo.mongo_client import MongoClient
 
 app = Flask(__name__)
 
-# connecting to cosmos db
-client = CosmosClient.from_connection_string("AccountEndpoint=https://li-c-436p4-server.documents.azure.com:443/;AccountKey=0Thk4cHi5Y0rLkelh4VB0Coxz2hpZY2fot03kUDh12rj6fW8oXMleZhwplJmQRJKQDzIb5F7LEndACDb8hGngw==")
-databases = client.list_databases(10) # testing print databases
-for database in databases:
-    print(database)
-
-# connecting to azure blob
-account_url = "https://cs41003200113807a1f.blob.core.windows.net"
-#credentials = ClientSecretCredential(tenant_id=)
-default_credential = DefaultAzureCredential()
+client = MongoClient("mongodb://li-c-436p4-server:0Thk4cHi5Y0rLkelh4VB0Coxz2hpZY2fot03kUDh12rj6fW8oXMleZhwplJmQRJKQDzIb5F7LEndACDb8hGngw==@li-c-436p4-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@li-c-436p4-server@")
+database = client["436p4DB"]
+dbCollect = database["storedData"]
 
 # Create the BlobServiceClient object
 blobContainerName = "436p4"
-blob_service_client = BlobServiceClient(account_url, credential=default_credential)
+blob_service_client = BlobServiceClient.from_connection_string("DefaultEndpointsProtocol=https;AccountName=cs41003200113807a1f;AccountKey=2Nljt9xH0Ry2YrCbWppi5C/lhx7CWxu6NIzeyQ9mAuLHyzIPG7aiGGrwKmm8GOe5lx0FsMV5MANj+AStdXc2xw==;EndpointSuffix=core.windows.net")
 container_client = blob_service_client.get_container_client("436p4")
 
 @app.route("/", methods=["POST", "GET"])
 def init_interface():
     if request.method == "POST":
+        # when prompted to load data
         if request.form["button"] == "Load Data":
-            # getting data
-            get_blob = BlobClient("https://css490.blob.core.windows.net", "lab4", "input.txt")
-            blob = get_blob.download_blob()
-            blob_text = blob.readall()
-            container_client.upload_blob("input.txt", blob_text, "BlockBlob")
-
-            text = str(blob_text)
-
-            # parsing data
-            text = text[2:len(text) - 1]
-            people = text.split("\\r\\n")
-            '''for ppl in people:
-                print(ppl)
-                print()'''
-
-            # storing data
-            for person in people:
-                attributes = person.split()
-                # lastName, firstName, attributes list
-                # attributes do not need to be separated, just need to be printed
-                # load to <key, value> format
-                # then, from there, store the rest of the attributes
-                #for attribute in attributes:
-                    # parse vartype and then value
-                    # store
-
-
-
-            # notify user
-            return '''<h1>data loaded</h1?'''
+            return redirect(url_for("load"))
         elif request.form["button"] == "Clear Data":
-            # delete everything from azure blob and cosmos db
-            return '''<h1>data cleared</h1>'''
+            return redirect(url_for("clear"))
         elif request.form["button"] == "Query":
-            input1 = request.form["fName"]
-            input = request.form["lName"]
-            # searching database or blob
-            # displaying results in new page
-            return f'''<h1>input: {input1} + {input}</h1>'''
+            fInput = request.form["fName"]
+            lInput = request.form["lName"]
+
+            if(fInput == "" and lInput == ""):
+                return redirect(url_for("queryAll"))
+            if(fInput == ""):
+                return redirect(url_for("queryL", lName = lInput))
+            elif(lInput == ""):
+                return redirect(url_for("queryF", fName = fInput))
+            # searching database
+            return redirect(url_for("query", fName = fInput, lName = lInput))
     else:
-        # when 'load' is hit, add data to database:
-        # from this url:
-        # https://css490.blob.core.windows.net/lab4/input.txt
-        # copy data into oject storage in cloud
-        # parse and load into database
-
-        # when 'clear' is hit, remove data from everywhere
-
         # when query:
         # if nothing in database, return error
         # return matches from database
@@ -96,6 +56,154 @@ def init_interface():
         <input type="text" id'="lName" name="lName"/>
         <input type="submit" name="button" value="Query"/>'''
 
-@app.route("/input")
-def input(input):
-    return '''<h1>input: {input}</h1>'''
+@app.route("/load")
+def load():
+    # connect to db
+    client = MongoClient("mongodb://li-c-436p4-server:0Thk4cHi5Y0rLkelh4VB0Coxz2hpZY2fot03kUDh12rj6fW8oXMleZhwplJmQRJKQDzIb5F7LEndACDb8hGngw==@li-c-436p4-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@li-c-436p4-server@")
+    database = client["436p4DB"]
+    dbCollect = database["storedData"]
+    
+    # Create the BlobServiceClient object
+    blobContainerName = "436p4"
+    blob_service_client = BlobServiceClient.from_connection_string("DefaultEndpointsProtocol=https;AccountName=cs41003200113807a1f;AccountKey=2Nljt9xH0Ry2YrCbWppi5C/lhx7CWxu6NIzeyQ9mAuLHyzIPG7aiGGrwKmm8GOe5lx0FsMV5MANj+AStdXc2xw==;EndpointSuffix=core.windows.net")
+    container_client = blob_service_client.get_container_client("436p4")
+
+    # getting data
+    get_blob = BlobClient("https://css490.blob.core.windows.net", "lab4", "input.txt")
+    blob = get_blob.download_blob()
+    blob_text = blob.readall()
+
+    # making copy in azure blob storage
+    container_client.upload_blob("input.txt", blob_text, "BlockBlob", overwrite=True)
+
+    # parsing data
+    text = str(blob_text)
+    text = text[2:len(text) - 1]
+    text = text.replace("\\t", " ")
+    people = text.split("\\r\\n")
+
+    # storing data
+    for person in people:
+        attributes = person.split() # getting list of attributes
+        # creating item with id, firstName, and lastName
+        new_item = {
+            "firstName": attributes[1],
+            "lastName": attributes[0],
+        }
+
+        # going through all attribtues and adding them to the item
+        for attribute in attributes[2:]:
+            keyValue = attribute.split("=")
+            new_item[keyValue[0]] = keyValue[1]
+
+        # adding to db
+        dbCollect.update_one({"firstName": attributes[1]}, {"$set": new_item}, upsert=True)
+
+    client.close()
+    blob_service_client.close()
+
+    # notify user
+    return '''<form action="#" method="post">
+    <h1>Data has been loaded! Queries are now updated.<h1>
+    <p>Go back to previous page to query.</p>'''
+
+@app.route("/clear")
+def clear():
+    # connect to db
+    client = MongoClient("mongodb://li-c-436p4-server:0Thk4cHi5Y0rLkelh4VB0Coxz2hpZY2fot03kUDh12rj6fW8oXMleZhwplJmQRJKQDzIb5F7LEndACDb8hGngw==@li-c-436p4-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@li-c-436p4-server@")
+    database = client["436p4DB"]
+    dbCollect = database["storedData"]
+    
+    # Create the BlobServiceClient object
+    blobContainerName = "436p4"
+    blob_service_client = BlobServiceClient.from_connection_string("DefaultEndpointsProtocol=https;AccountName=cs41003200113807a1f;AccountKey=2Nljt9xH0Ry2YrCbWppi5C/lhx7CWxu6NIzeyQ9mAuLHyzIPG7aiGGrwKmm8GOe5lx0FsMV5MANj+AStdXc2xw==;EndpointSuffix=core.windows.net")
+    container_client = blob_service_client.get_container_client("436p4")
+
+    # delete everything from azure blob and cosmos db
+    if container_client.get_blob_client("input.txt").exists():
+        container_client.delete_blob("input.txt")
+        dbCollect.delete_many({})
+        client.close()
+        blob_service_client.close()
+        return '''<h1>Data cleared.</h1>'''
+    else:
+        client.close()
+        blob_service_client.close()
+        return '''<h1>Currently no data to clear.</h1>'''
+
+@app.route("/queryAll")
+def queryAll():
+    # connect to db
+    client = MongoClient("mongodb://li-c-436p4-server:0Thk4cHi5Y0rLkelh4VB0Coxz2hpZY2fot03kUDh12rj6fW8oXMleZhwplJmQRJKQDzIb5F7LEndACDb8hGngw==@li-c-436p4-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@li-c-436p4-server@")
+    database = client["436p4DB"]
+    dbCollect = database["storedData"]
+    results = dbCollect.find({})
+    resultString = "<h1>Results:</h1>"
+    for result in results:
+        resultString += "<p>" + str(result) + "</p>"
+
+    client.close()
+    return resultString
+
+
+@app.route("/query/<string:fName>_<string:lName>")
+def query(fName, lName):
+    # connect to db
+    client = MongoClient("mongodb://li-c-436p4-server:0Thk4cHi5Y0rLkelh4VB0Coxz2hpZY2fot03kUDh12rj6fW8oXMleZhwplJmQRJKQDzIb5F7LEndACDb8hGngw==@li-c-436p4-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@li-c-436p4-server@")
+    database = client["436p4DB"]
+    dbCollect = database["storedData"]
+
+    # searching database
+    results = None
+    if(fName == ""):
+        results = dbCollect.find({"lastName": lName})
+    elif(lName == ""):
+        results = dbCollect.find({"firstName": fName})
+    else:
+        results = dbCollect.find({"firstName": fName, "lastName": lName})
+    resultString = "<h1>Results matching " + lName + ", " + fName + ":</h1>"
+    for result in results:
+        resultString += "<p>" + str(result) + "</p>"
+
+    client.close()
+    
+    # displaying results in new page
+    return resultString
+
+@app.route("/queryF/<string:fName>_")
+def queryF(fName):
+    # connect to db
+    client = MongoClient("mongodb://li-c-436p4-server:0Thk4cHi5Y0rLkelh4VB0Coxz2hpZY2fot03kUDh12rj6fW8oXMleZhwplJmQRJKQDzIb5F7LEndACDb8hGngw==@li-c-436p4-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@li-c-436p4-server@")
+    database = client["436p4DB"]
+    dbCollect = database["storedData"]
+
+    # searching database
+    results = None
+    results = dbCollect.find({"firstName": fName})
+    resultString = "<h1>Results matching " + fName + ":</h1>"
+    for result in results:
+        resultString += "<p>" + str(result) + "</p>"
+
+    client.close()
+    
+    # displaying results in new page
+    return resultString
+
+@app.route("/queryL/_<string:lName>")
+def queryL(lName):
+    # connect to db
+    client = MongoClient("mongodb://li-c-436p4-server:0Thk4cHi5Y0rLkelh4VB0Coxz2hpZY2fot03kUDh12rj6fW8oXMleZhwplJmQRJKQDzIb5F7LEndACDb8hGngw==@li-c-436p4-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@li-c-436p4-server@")
+    database = client["436p4DB"]
+    dbCollect = database["storedData"]
+
+    # searching database
+    results = dbCollect.find({"lastName": lName})
+    resultString = "<h1>Results matching " + lName + ":</h1>"
+    for result in results:
+        print(result)
+        resultString += "<p>" + str(result) + "</p>"
+
+    client.close()
+    
+    # displaying results in new page
+    return resultString
